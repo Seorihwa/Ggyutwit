7fbfe2dc066f50bc35d6ba7a22c56d4253fbea2e
Seorihwa <lovetoyou32@gmail.com>
2021-08-26 23:40:25 +0900

    Chapter 5 page 123 finished

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,60 @@
-const Home = () => <span>Home</span>;
+import { dbService } from "fbase";
+import { useEffect, useState } from "react";
+
+const Home = () => {
+    const [ggyutweet, setGgyutweet] = useState("");
+    const [ggyutweets, setGgyutweets] = useState([]);
+
+    const getGgyutweets = async () => {
+        const dbGgyutweets = await dbService.collection("ggyutweets").get();
+        dbGgyutweets.forEach((document) => {
+            const ggyutweetObject = { ...document.data(), id: document.id };
+            setGgyutweets((prev) => [ggyutweetObject, ...prev])
+        });
+    };
+
+    useEffect(() => {
+        getGgyutweets();
+    }, []);
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await dbService.collection("ggyutweets").add({
+            text: ggyutweet,
+            createdAt: Date.now(),
+        });
+        setGgyutweet("");
+    };
+
+    const onChange = (event) => {
+        event.preventDefault();
+        const {
+            target: { value },
+        } = event;
+        setGgyutweet(value);
+    };
+
+    return (
+        <>
+            <form onSubmit={onSubmit}>
+                <input
+                    value={ggyutweet}
+                    onChange={onChange}
+                    type="text"
+                    placeholder="What's on your mind?"
+                    maxLength={120}
+                />
+                <input type="submit" value="Ggyutweet" />
+            </form>
+            <div>
+                {ggyutweets.map((ggyutweet) => (
+                    <div key={ggyutweet.id}>
+                        <h4>{ggyutweet.text}</h4>
+                    </div>
+                ))}
+            </div>
+        </>
+    );
+};
 
 export default Home;
